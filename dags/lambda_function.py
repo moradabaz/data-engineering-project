d57afc1e3e8cee6461191dec93b0881353d2e3a1
morad <morad.abdou@idrica.com>
2024-08-26 20:08:02 +0200

    update log

--- dags/lambda_function.py
+++ dags/lambda_function.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
 from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 from datetime import datetime
 from dotenv import find_dotenv, load_dotenv, set_key
 import os
@@ -13,68 +14,56 @@ default_args = {
     'retries': 1
 }
 
-load_dotenv()
+load_dotenv(dotenv_path='/opt/airflow/.env', override=True)
 
+def save_token_f(token):
+    Variable.delete('API_TOKEN')
+    Variable.set("API_TOKEN", token)
+    print(f"Token guardado: {token}")
 
-def generate_token(**kwargs):
-    url = 'https://test.api.amadeus.com/v1/security/oauth2/token'  # URL de la API para obtener el token
+def save_token_to_env(**kwargs):
+    task_instance = kwargs['ti']
+    response  = task_instance.xcom_pull(task_ids='generate_token_task')
+    response = json.loads(response)
+    token = response['token'].strip('"')
+    print(f'token received: {token}')
     
-    client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
-    
-    print(f'client id -> {client_id}')
-        
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    data = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret
-    }
-
     try:
-        # Hacer la solicitud POST para obtener el token
-        response = requests.post(url, headers=headers, data=data)
-        
-        # Verificar que la solicitud fue exitosa
-        response.raise_for_status()
+        set_key(dotenv_path=find_dotenv(), key_to_set='API_TOKEN', value_to_set=token)
+    except Exception as e:
+        print(f'Error al guardar el token en el entorno: {e}')
+        return 'success'
+    
 
-        # Obtener los datos de la respuesta en formato JSON
-        token_data = response.json()
-        access_token = token_data.get('access_token')
-        
-        env_path = find_dotenv()
-        set_key(env_path, 'API_TOKEN', access_token)
-        print(f'Token guardado en {env_path}')
-
-        return access_token
-
-    except requests.exceptions.HTTPError as http_err:
-        print(f'Error HTTP: {http_err}')
-    except Exception as err:
-        print(f'Error: {err}')
 
 with DAG(dag_id='lambda_example_dag', default_args=default_args, schedule_interval=None, max_active_runs=1) as dag:
     
-    generate_token_task = PythonOperator(
-        task_id='generate_token',
-        python_callable=generate_token,
+    payload = {"client_id": os.getenv('CLIENT_ID'), "client_secret": os.getenv('CLIENT_SECRET')}
+    playload_bytes = json.dumps(payload).encode('utf-8')
+    
+    generate_token_task = LambdaInvokeFunctionOperator(
+        task_id='generate_token_task',
+        function_name='generate-token-function',
+        log_type='Tail',
+        payload=playload_bytes,
+        aws_conn_id='aws_default',  # Configura tu conexión a AWS en Airflow
+        region_name='eu-south-2'
+    )
+
+    save_token_task = PythonOperator(
+        task_id='save_token_task',
+        python_callable=save_token_to_env,
         provide_context=True,
+        dag=dag
     )
     
-    payload = { 'token': os.getenv('API_TOKEN') }
-    payload_bytes = json.dumps(payload).encode('utf-8')
-
-
     invoke_lambda = LambdaInvokeFunctionOperator(
         task_id='invoke_lambda',
         function_name='my-first-lambda',  # Reemplaza con el nombre de tu Lambda
-        payload=payload_bytes,
+        payload=json.dumps({ 'token': os.environ['API_TOKEN']}).encode('utf-8'),
         log_type='Tail',
         aws_conn_id='aws_default',  # Configura tu conexión a AWS en Airflow
         region_name='eu-south-2'
     )
 
-    generate_token_task >> invoke_lambda
+    generate_token_task >> save_token_task >> invoke_lambda
